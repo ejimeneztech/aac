08023a9f30fbe8dcf07af0cae96a744b83594008
Emilio Jimenez <emiliojimenez@Emilios-MBP.lan>
2022-03-02 23:19:16 -0800

    working on making button dynanmic

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from ui import UserInterface
 from api import data
+from speaker import Speaker
 
+post_list = []
+for post in data:
+    post_text = post["text"]
+    post_url = post["url"]
+    new_speaker = Speaker(post_text, post_url)
 
 
 UI = UserInterface()
