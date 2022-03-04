37f279c4539dc21a9aca54b5afe89e235f8f6c28
Emilio Jimenez <emiliojimenez@Emilios-MBP.lan>
2022-03-03 22:11:10 -0800

    made a simple prototype; figuring out how to stream the audio from s3

--- speaker.py
+++ speaker.py
@@ -1,4 +1,13 @@
+from playsound import playsound
+
+
+
 class Speaker:
     def __init__(self, speaker_text, speaker_url):
         self.text = speaker_text
         self.url = speaker_url
+    
+
+    def play_audio(self):
+        playsound('https://s3-us-west-2.amazonaws.com/audiopost-test/fec2458a-deff-4ef4-bc1a-8901f238d847.mp3')
+        
--- ui.py
+++ ui.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from tkinter import *
 
 THEME_COLOR = "#375362"
@@ -10,11 +11,13 @@ class UserInterface:
         self.window.title("AAC")
         self.window.config(padx=150, pady=100, bg=THEME_COLOR)
 
-        self.text_box = Canvas(width=450, height=50, bg="white")
-        self.output_text = self.text_box.create_text(150, 125, text="Test", width=280, font=("Arial", 20, "italic"))
-        self.text_box.grid(row=0, column=0, columnspan=2)
+        # self.text_box = Canvas(width=450, height=50, bg="white")
+        # self.text = self.text_box.create_text(150, 125, text=f"{self.speaker.text}", width=280, font=("Arial", 20, "italic"))
+        # self.text_box.grid(row=0, column=0, columnspan=2)
 
-        self.button = Button(text=f"{self.speaker.text}")
+
+        self.button = Button(text=f"{self.speaker.text}", command=self.speaker.play_audio)
         self.button.grid(row=2, column=0, pady=10)
+
 
         self.window.mainloop()
